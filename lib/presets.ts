a005f155d211b823252eeb5c58e3c15daabb09d2
drna lab <39486179+gdx7@users.noreply.github.com>
2025-08-13 17:02:47 +0200

    Create presets.ts

---
+++ lib/presets.ts
@@ -0,0 +1,17 @@
+// lib/presets.ts
+export const PRESETS = {
+  // globalMAP & csMAP use this
+  interactions: [
+    {
+      label: "interaction_EC.csv (Vercel Blob)",
+      url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/interaction_EC.csv",
+    },
+  ],
+  // pairMAP & foldMAP use this
+  chimeras: [
+    {
+      label: "LR_chimera_EC.bed (Vercel Blob)",
+      url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/LR_chimera_EC.bed",
+    },
+  ],
+};

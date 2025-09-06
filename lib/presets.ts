71f8874e2c52c904a603a74446058adfe6d1566f
drna lab <39486179+gdx7@users.noreply.github.com>
2025-09-06 14:51:27 +0200

    Update presets.ts

--- lib/presets.ts
+++ lib/presets.ts
@@ -13,7 +13,7 @@ export const PRESETS = {
     { label: "Chimeras — EC", url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/LR_chimera_EC.bed" },
     { label: "Structure — EC", url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/chimera_EC_all.bed" },
     { label: "Chimeras — SA", url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/LR_chimera_SA.bed" },
-    { label: "Structure — SA", url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/chimeras_SA_all.bed" },
+    { label: "Structure — SA", url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/chimera_SA_all.bed" },
     { label: "Chimeras — SS", url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/LR_chimeras_SS.bed" },
     { label: "Chimeras — MX", url: "https://6xaweu7axahancgd.public.blob.vercel-storage.com/LR_chimera_MX.bed" },
   ],
